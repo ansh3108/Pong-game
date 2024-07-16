53365fda64f3c921eb56d70204e43c73b5886704
Ansh Kumar <115077985+ansh3108@users.noreply.github.com>
2024-07-16 12:40:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -44,3 +44,91 @@ window.onload = function() {
     requestAnimationFrame(update);
     document.addEventListener("keyup", movePlayer);
 }
+function update() {
+    requestAnimationFrame(update);
+    context.clearRect(0, 0, board.width, board.height);
+
+    context.fillStyle = "skyblue";
+    let nextPlayer1Y = player1.y + player1.velocityY;
+    if (!outOfBounds(nextPlayer1Y)) {
+        player1.y = nextPlayer1Y;
+    }
+    context.fillRect(player1.x, player1.y, playerWidth, playerHeight);
+
+    let nextPlayer2Y = player2.y + player2.velocityY;
+    if (!outOfBounds(nextPlayer2Y)) {
+        player2.y = nextPlayer2Y;
+    }
+    context.fillRect(player2.x, player2.y, playerWidth, playerHeight);
+
+    context.fillStyle = "white";
+    ball.x += ball.velocityX;
+    ball.y += ball.velocityY;
+    context.fillRect(ball.x, ball.y, ballWidth, ballHeight);
+
+    if (ball.y <= 0 || (ball.y + ballHeight >= boardHeight)) {
+        ball.velocityY *= -1;
+    }
+
+    if (detectCollision(ball, player1)) {
+        if (ball.x <= player1.x + player1.width) {
+            ball.velocityX *= -1;
+        }
+    } else if (detectCollision(ball, player2)) {
+        if (ball.x + ballWidth >= player2.x) {
+            ball.velocityX *= -1;
+        }
+    }
+
+    if (ball.x < 0) {
+        player2Score++;
+        resetGame(1);
+    } else if (ball.x + ballWidth > boardWidth) {
+        player1Score++;
+        resetGame(-1);
+    }
+
+    context.font = "45px sans-serif";
+    context.fillText(player1Score, boardWidth/5, 45);
+    context.fillText(player2Score, boardWidth*4/5 - 45, 45);
+
+    for (let i = 10; i < board.height; i += 25) {
+        context.fillRect(board.width / 2 - 10, i, 5, 5);
+    }
+}
+
+function outOfBounds(yPosition) {
+    return (yPosition < 0 || yPosition + playerHeight > boardHeight);
+}
+
+function movePlayer(e) {
+    if (e.code == "KeyW") {
+        player1.velocityY = -3;
+    } else if (e.code == "KeyS") {
+        player1.velocityY = 3;
+    }
+
+    if (e.code == "ArrowUp") {
+        player2.velocityY = -3;
+    } else if (e.code == "ArrowDown") {
+        player2.velocityY = 3;
+    }
+}
+
+function detectCollision(a, b) {
+    return a.x < b.x + b.width &&
+           a.x + a.width > b.x &&
+           a.y < b.y + b.height &&
+           a.y + a.height > b.y;
+}
+
+function resetGame(direction) {
+    ball = {
+        x : boardWidth/2,
+        y : boardHeight/2,
+        width: ballWidth,
+        height: ballHeight,
+        velocityX : direction,
+        velocityY : 2
+    }
+}
